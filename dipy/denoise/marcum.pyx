6c0aac3b69ac49568f2e6665b2d5643097f5e499
Samuel St-Jean <stjeansam@gmail.com>
2013-12-09 15:57:34 +0100

    Finished cython

--- dipy/denoise/marcum.pyx
+++ dipy/denoise/marcum.pyx
@@ -1,61 +1,66 @@
 from __future__ import division
 import numpy as np
 
+from scipy.special import iv, factorial
+
 cimport numpy as np
 cimport cython
 
-DTYPE = np.float32
-ctypedef np.float32_t DTYPE_t
 
-@cython.boundscheck(False) # turn off bounds-checking for entire function
+@cython.boundscheck(False)
+@cython.wraparound(False)
 def _marcumq(float a, float b, int M, float eps=10**-10):
 
     if b == 0:
         return 1
 
+    cdef np.ndarray ka = np.arange(M, dtype=np.int)
+
     if a == 0:
-
-        cdef int k = np.arange(M)
-
-        return np.exp(-b**2/2) * np.sum(b**(2*k) / (2**k * factorial(k)))
+        return np.exp(-b**2/2) * np.sum(b**(2*ka) / (2**ka * factorial(ka)))
 
     cdef float z = a * b
-    cdef int k = 0
+    cdef int k
+    cdef int s
+    cdef int c
+    cdef float x
+    cdef float d
+    cdef float S
+    cdef float t
 
     if a < b:
 
-        cdef int s = 1
-        cdef int c = 0
-        cdef float x = a/b
-        cdef float d = x
-        cdef float S = iv(0, z) * np.exp(-z)
+        s = 1
+        c = 0
+        x = a/b
+        d = x
+        S = iv(0, z) * np.exp(-z)
 
         for k in range(1, M):
-            cdef float t = (d + 1/d) * iv(k, z) * np.exp(-z)
-            cdef float S += t
-            cdef float d *= x
+            t = (d + 1/d) * iv(k, z) * np.exp(-z)
+            S += t
+            d *= x
 
         k += 1
 
     else:
-        cdef int s = -1
-        cdef int c = 1
-        cdef float x = b/a
-        cdef int k = M
-        cdef float d = x**M
-        cdef float S = 0
-        cdef float t = 1
+        s = -1
+        c = 1
+        x = b/a
+        k = M
+        d = x**M
+        S = 0
+        t = 1
 
-    cdef bool condition = True
+    cdef bint condition = True
 
     while (condition):
 
-        t = d * iv(np.abs(k), z) * np.exp(-z)
+        t = d * iv(k, z) * np.exp(-z)
         S += t
         d *= x
         k += 1
 
         condition = np.abs(t/S) > eps
-
 
     return c + s * np.exp(-0.5 * (a-b)**2) * S
