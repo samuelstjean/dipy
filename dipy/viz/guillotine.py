7fd6e9b810bd4242a078b3e9e88ab33f23c1f66e
gauvinalexandre <gauvinalexandre@gmail.com>
2014-08-21 01:22:22 -0400

    Added support of world coordinate system

--- dipy/viz/guillotine.py
+++ dipy/viz/guillotine.py
@@ -1,9 +1,8 @@
-# TODO: World coordinate system
 # TODO: Add support for LUT input
 # TODO: Add key events management (opacity,XYZ axes, translation, rotation
 # TODO: Adapt assertions to dipy style
 # TODO: Support VTK6
-# TODO: Put utils in fvtk_utils
+# TODO: Find a place to put utils.py
 
 import vtk
 import utils
@@ -16,9 +15,6 @@ class Guillotine:
         self.nb_data_volumes = 0
         self.blender = vtk.vtkImageBlend()
         self.renderer = vtk.vtkRenderer()
-        
-        # Set the renderer
-        self.renderer.SetBackground(0.0, 0.0, 0.0)
 
     def _myCallback(self, obj, event):
         # Parameters:
@@ -39,28 +35,57 @@ class Guillotine:
 
         self.renderer.AddActor(actor)
 
-    def add_data_volume(self, data, opacity=0.5):
+    def add_data_volume(self, data, opacity=0.5, affine=None):
         # Parameters:
         #     data: Numpy NdArray
         #     opacity: float
+        #     affine: 4x4 Numpy NdArray
         #
         # Function: (required)
-        #     Adds a data volume to the blending with its
-        #     corresponding opacity. Every data volume added should have the
-        #     same extent and the one with the most components added first.
+        #     Adds a data volume to the blending with its corresponding
+        #     opacity. Every data volume added should have the same extent
+        #     and the one with the most components added first. The affine
+        #     transformation is applied if present.
 
         # Transform data into vtk tangible object
-        data_volume = utils.ndarray2vtkImageData(data)
+        image_data = utils.ndarray2vtkImageData(data)
 
+        # Set the transform
+        transform = vtk.vtkTransform()
+        if affine is not None:
+            transform_matrix = vtk.vtkMatrix4x4()
+            transform_matrix.DeepCopy((
+                affine[0][0], affine[0][1], affine[0][2], affine[0][3],
+                affine[1][0], affine[1][1], affine[1][2], affine[1][3],
+                affine[2][0], affine[2][1], affine[2][2], affine[2][3],
+                affine[3][0], affine[3][1], affine[3][2], affine[3][3]))
+            transform.SetMatrix(transform_matrix)
+            transform.Inverse()
+
+        # Set the reslicing
+        image_reslice = vtk.vtkImageReslice()
+        image_reslice.SetInput(image_data)
+        image_reslice.SetResliceTransform(transform)
+        image_reslice.AutoCropOutputOn()
+        image_reslice.SetInterpolationModeToCubic()
+
+        # Set the transformed image
+        transformed_image = vtk.vtkImageData()
+        transformed_image = image_reslice.GetOutput()
+        transformed_image.Update()
+
+        # Check if the extent is compatible with previous volumes
         if self.nb_data_volumes > 0:
-            assert (data_volume.GetExtent() ==
+            assert (transformed_image.GetExtent() ==
                     self.blender.GetOutput().GetWholeExtent()), \
-                "data_volume extent doesn't fit the actual extent. (" + \
-                str(data_volume.GetExtent()) + \
+                "image data extent doesn't fit the actual extent. (" + \
+                str(transformed_image.GetExtent()) + \
                 " vs actual " + \
                 str(self.blender.GetOutput().GetWholeExtent()) + \
                 ")"
-        self.blender.SetInput(self.nb_data_volumes, data_volume)
+
+        # Blend volumes
+        self.blender.SetInput(self.nb_data_volumes, transformed_image)
         self.blender.SetOpacity(self.nb_data_volumes, opacity)
         self.blender.UpdateWholeExtent()
         self.nb_data_volumes += 1
@@ -74,12 +99,15 @@ class Guillotine:
         assert (self.nb_data_volumes > 0), \
             "No data volume, use function add_data_volume."
         data_volume = self.blender.GetOutput()
-
-        # Get volume properties
-        xdim, ydim, zdim = data_volume.GetDimensions()
-        xmin, xmax, ymin, ymax, zmin, zmax = data_volume.GetExtent()
-        origin = [(xmax+xmin)/2, (ymax+ymin)/2, (zmax+zmin)/2]
-        initial_normal = [1, 0, 0]
+        extent = data_volume.GetExtent()
+        ori = data_volume.GetOrigin()
+        bbox = (
+            (extent[0] + ori[0]),
+            (extent[1] + ori[0]),
+            (extent[2] + ori[1]),
+            (extent[3] + ori[1]),
+            (extent[4] + ori[2]),
+            (extent[5] + ori[2]))
 
         # Set cutter filter
         cutter = vtk.vtkCutter()
@@ -94,12 +122,12 @@ class Guillotine:
         cutter_actor = vtk.vtkActor()
         cutter_actor.SetMapper(cutter_mapper)
         self.add_actor(cutter_actor)
-        
+
         # Set render window
         render_window = vtk.vtkRenderWindow()
         render_window.SetSize(800, 600)
         render_window.AddRenderer(self.renderer)
-        
+
         # Set interactor
         interactor = vtk.vtkRenderWindowInteractor()
         interactor.SetRenderWindow(render_window)
@@ -116,24 +144,33 @@ class Guillotine:
         plane_widget.OutsideBoundsOff()
         plane_widget.SetPlaceFactor(1)
         plane_widget.PlaceWidget()
-        plane_widget.SetOrigin(
-            origin[0],
-            origin[1],
-            origin[2])
-        plane_widget.SetNormal(
-            initial_normal[0],
-            initial_normal[1],
-            initial_normal[2])
+        plane_widget.SetNormal(0, 0, 1)
         plane_widget.GetPlane(self.plane)
+
+        # Set axes
+        axes = vtk.vtkCubeAxesActor()
+        axes.SetBounds(bbox[0], bbox[1], bbox[2], bbox[3], bbox[4], bbox[5])
+        axes.SetTickLocationToBoth()
+        axes.SetXUnits("mm")
+        axes.SetYUnits("mm")
+        axes.SetZUnits("mm")
+        axes.SetFlyModeToOuterEdges()
+        axes.SetCamera(self.renderer.GetActiveCamera())
+        self.add_actor(axes)
+
+        # Set the renderer
+        self.renderer.SetBackground(0.18, 0.18, 0.18)
+        self.renderer.GetActiveCamera().SetPosition(0, 0, -1)
+        self.renderer.ResetCamera()
 
         # Render scene
         self.renderer.Render()
 
         # Initialize interactor
         interactor.Initialize()
-        
+
         # Render window
         render_window.Render()
-        
+
         # Start interactor
         interactor.Start()
