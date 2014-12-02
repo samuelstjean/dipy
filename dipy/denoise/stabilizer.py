e7cb2c8f716f61a95398e5901b833943d5a84f92
Samuel St-Jean <samuel.st-jean@usherbrooke.ca>
2014-12-02 12:51:42 -0500

    stuff

--- dipy/denoise/stabilizer.py
+++ dipy/denoise/stabilizer.py
@@ -151,7 +151,7 @@ def main():
 
     pool = Pool(processes=n_cores)
     arglist=[(data_vox, m_hat_vox, sigma_vox, N_vox) for data_vox, m_hat_vox, sigma_vox, N_vox in zip(data, m_hat, repeat(sigma_mode), repeat(N))]
-    data_stabilized = pool.map(helper, arglist, chunksize=chunk_size)
+    data_stabilized = pool.map_async(helper, arglist, chunksize=chunk_size)
     #print(arglist[0], 'bla')
     #out =  pool.map(helper, arglist)
     #data_stabilized = np.asarray(data_stabilized).reshape(data.shape)
