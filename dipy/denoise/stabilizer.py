17ff7abe816151fa96815f18ca48b619a3ad2316
Samuel St-Jean <samuel.st-jean@usherbrooke.ca>
2014-12-02 11:46:26 -0500

    stuff

--- dipy/denoise/stabilizer.py
+++ dipy/denoise/stabilizer.py
@@ -61,7 +61,7 @@ def helper(arglist):
     for idx in ndindex(data.shape):
         #print(data[idx],m_hat[idx], sigma, N, "2nd")
         #print(idx)
-        eta = _fixed_point_finder(m_hat[idx], sigma, N)
+        eta = m_hat[idx]#_fixed_point_finder(m_hat[idx], sigma, N)
         #print(eta,"3rd")
         out[idx] = _chi_to_gauss(data[idx], eta, sigma, N)
 
